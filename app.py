009f3a92819ded9c98dc134f3f9657e36162be5a
shaikmasthan7y <shaikmasthan7y@gmail.com>
2025-09-19 22:25:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from AWS DevOps Project!"
+    return "Hello from AWS DevOpss Project!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
